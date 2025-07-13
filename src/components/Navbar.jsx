964e0b007982762cb5e541aeac683bad1de3fc4d
Satyam-R4j <satyamraj418@gmail.com>
2025-07-13 23:16:38 +0530

    Working on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,17 @@ import { TrendingUpDown } from "lucide-react";
 const Navbar = () => {
   return (
     <>
-      <div className="nav flex justify-between items-center  h-[90px] bg-zinc-900 " style={{padding:"0px 150px"}}>
-        <div className="logo">
+      <div
+        className="nav flex justify-between items-center  h-[90px] bg-zinc-900 "
+        style={{ padding: "0px 150px" }}
+      >
+        <div className="logo flex items-center gap-2">
           {" "}
-          <TrendingUpDown size={30} color="#10B981"/>
+          <TrendingUpDown size={30} color="#10B981" />
+          <span className="text-2xl  text-white ">
+            <span className="text-2xl font-bold text-gray-400 ">Code</span>
+            <span className="text-2xl font-semibold  bg-green-700 rounded-lg text-black ">Clarify</span>
+          </span>
         </div>
       </div>
     </>
